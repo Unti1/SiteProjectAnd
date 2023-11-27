7691c5ad8a588c54ffa6003580ccd59bd668b469
Unti1 <kostyuchenko.work@gmail.com>
2023-11-27 22:07:01 +0300

    ...

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'), # http://127.0.0.1:8000/
+    # path('posts', views.post_list, name='post_list'), # http://127.0.0.1:8000/posts
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    ]
